4e483fc76c2acb3637012701f7911ae8e52f1b85
OuChiaYun <j312079@gmail.com>
2023-12-14 08:56:08 +0800

    add new ml of forest with cross validation

--- src/parse.R
+++ src/parse.R
@@ -4,7 +4,7 @@ myLoad <- champ.load("train/stage_III")#, SampleCutoff = 0.2)#, arraytype = "EPI
 
 myNorm <- champ.norm(beta=myLoad$beta,plotBMIQ=FALSE,cores=5)#, arraytype = "EPIC")
 
-write.csv(myNorm,file="result/all_beta_normalized_stage_III.csv",quote=F,row.names=T)
+write.csv(myNorm,file="all_beta_normalized_stage_450k_1213.csv",quote=F,row.names=T)
 
 myDMP <- champ.DMP(beta=myNorm, pheno=myLoad$pd$Sample_Group)#, arraytype = "EPIC")
 
