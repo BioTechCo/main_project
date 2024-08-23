61abe300553f6207270823205b71778b3ceadc81
vincent <hankggg2003xd@gmail.com>
2024-08-23 10:26:27 +0800

    [refactor] refactor go clustering

--- R-scripts/GO_clustering.R
+++ R-scripts/GO_clustering.R
@@ -1,110 +1,85 @@
-# nolint start
-# 載入需要的套件
 library(GOSemSim)
 library(org.Hs.eg.db)
-library(stats)
-library(dendextend)
-# 讀取CSV檔案並將基因名稱(symbol)轉換成Entrez Gene IDs
-data <- read.csv("result/gene_list.txt")
-entrez_ids <- mapIds(org.Hs.eg.db, keys=data$gene, keytype="SYMBOL", column="ENTREZID")
+
+input_file <- "breast/result/GSE89093_nc/train100/dbeta_hyper_TSS_0.02.csv"
+cluster_num <- 5
+output_pdf <- "breast/result/GSE89093_nc/train100/dendrogram.pdf"
+output_csv <- "breast/result/GSE89093_nc/train100/distance_matrix.csv"
+
+# read the input file
+data <- read.csv(input_file, header = TRUE, stringsAsFactors = FALSE)
+entrez_ids <- mapIds(
+  org.Hs.eg.db,
+  keys = data$gene,
+  keytype = "SYMBOL",
+  column = "ENTREZID"
+)
+
+# remove NA values
 entrez_ids <- entrez_ids[!is.na(entrez_ids)]
 
-# 創建GO資料物件，考慮所有三個GO分支
+# load GO data for biological process (BP), cellular component (CC), and molecular function (MF)
 d_all <- lapply(c("BP", "CC", "MF"), function(ont) {
-    godata('org.Hs.eg.db', ont=ont, computeIC=FALSE)
+  godata(annoDb = "org.Hs.eg.db", ont = ont, computeIC = FALSE)
 })
 
-# 計算基於所有三個GO分支的語義相似度
+# calculate semantic similarity matrices
 sim_matrices <- lapply(d_all, function(d) {
-    mgeneSim(entrez_ids, semData=d,drop="", measure="Wang",verbose = TRUE)
+  mgeneSim(entrez_ids, semData = d, drop = "", measure = "Wang", verbose = TRUE)
 })
+sim_matrix_bp <- sim_matrices[[1]]
+sim_matrix_cc <- sim_matrices[[2]]
+sim_matrix_mf <- sim_matrices[[3]]
 
-# 現在有三個矩陣：基於BP、CC和MF的語義相似度
-sim_matrix_BP <- sim_matrices[[1]]
-sim_matrix_CC <- sim_matrices[[2]]
-sim_matrix_MF <- sim_matrices[[3]]
+# gather all unique genes
+all_genes <- unique(
+  c(
+    row.names(sim_matrix_bp),
+    row.names(sim_matrix_cc),
+    row.names(sim_matrix_mf)
+  )
+)
 
-# 獲取所有的基因名稱
-all_genes <- unique(c(row.names(sim_matrix_BP), row.names(sim_matrix_CC), row.names(sim_matrix_MF)))
-
-# 初始化一個全為NA的矩陣
+# create a combined matrix
 combined_matrix <- matrix(NA, length(all_genes), length(all_genes))
 rownames(combined_matrix) <- all_genes
 colnames(combined_matrix) <- all_genes
 
-# 填充每個相似度矩陣的值
+# fill the combined matrix with the similarity matrices
 fill_matrix <- function(main_matrix, sub_matrix) {
   rows <- rownames(sub_matrix)
   cols <- colnames(sub_matrix)
   main_matrix[rows, cols] <- sub_matrix
   return(main_matrix)
 }
+combined_matrix <- fill_matrix(combined_matrix, sim_matrix_bp)
+combined_matrix <- fill_matrix(combined_matrix, sim_matrix_cc)
+combined_matrix <- fill_matrix(combined_matrix, sim_matrix_mf)
 
-combined_matrix <- fill_matrix(combined_matrix, sim_matrix_BP)
-combined_matrix <- fill_matrix(combined_matrix, sim_matrix_CC)
-combined_matrix <- fill_matrix(combined_matrix, sim_matrix_MF)
+# transform Entrez IDs to gene symbols using mapIds
+gene_symbols <- mapIds(
+  org.Hs.eg.db,
+  keys = row.names(combined_matrix),
+  keytype = "ENTREZID",
+  column = "SYMBOL"
+)
 
-# 如果你希望將NA值替換為0：
-# combined_matrix[is.na(combined_matrix)] <- 0
-
-# 使用mapIds轉換Entrez IDs為基因符號
-gene_symbols <- mapIds(org.Hs.eg.db, keys=row.names(combined_matrix), keytype="ENTREZID", column="SYMBOL")
-
-# 更新combined_matrix的行名和列名
+# update row and column names of combined_matrix
 rownames(combined_matrix) <- gene_symbols
 colnames(combined_matrix) <- gene_symbols
 
-# 如果你想保存這個矩陣：
-write.csv(combined_matrix, file="combined_matrix_symbols.csv", row.names=TRUE)
+# calculate the distance matrix
+distance_matrix <- as.dist(1 - combined_matrix)
 
+# check for NA, NaN, and Infinite values
+cat("Number of NA values: ", sum(is.na(distance_matrix)), "\n")
+cat("Number of NaN values: ", sum(is.nan(distance_matrix)), "\n")
+cat("Number of Infinite values: ", sum(is.infinite(distance_matrix)), "\n")
 
-# 計算距離矩陣
-distance_matrix <- as.dist(1-combined_matrix)
+# replace NA, NaN, and Infinite values with 1
+distance_matrix[is.na(distance_matrix) | is.nan(distance_matrix) |
+    is.infinite(distance_matrix)
+] <- 1
 
-# 檢查是否存在NA、NaN或Inf的值
-sum(is.na(distance_matrix))
-sum(is.nan(distance_matrix))
-sum(is.infinite(distance_matrix))
-
-# 將非正常值替換為1（表示最大距離）
-distance_matrix[is.na(distance_matrix) | is.nan(distance_matrix) | is.infinite(distance_matrix)] <- 1
-
-# 進行層次聚類
-hc <- hclust(distance_matrix, method="ward.D2") # 這裡我使用的是average linkage方法，但你可以根據需要選擇其他方法
-
-# 將hclust物件轉換為dendrogram物件
-dend <- as.dendrogram(hc)
-
-# 使用cutree將層次聚類結果切成k個集群
-k <- 5 # 例如，我們切成3個集群
-
-clusters <- cutree(hc, k)# clusters矩陣現在包含每個基因的集群分配
-
-# 定義三種顏色
-colors_vector <- c("red", "blue", "green", "yellow", "purple")
-
-# 使用color_branches著色dendrogram
-colored_dend <- dend %>% 
-    set("labels_col", colors_vector[clusters]) %>%
-    set("branches_k_color", k = k) %>%
-    set("labels_cex", 0.5)
-
-# 打開一個更高的圖形裝置
-pdf("Dendrogram.pdf", width=10, height=80)
-
-# 繪製樹狀圖
-colored_dend %>%
-  set("labels_cex", 0.5) %>%  # 減小標籤大小
-  plot(horiz = TRUE, main = "Gene Distance Tree with Colored Clusters") 
-
-#如果pdf印不出東西表示這個寫錯了，ylim = c(0, 0.5)去掉後就可以跑出來了
-
-#colored_dend %>%
-#set("labels_cex", 0.5) %>%  # 減小標籤大小
-#plot(horiz = TRUE, main = "Gene Distance Tree with Colored Clusters")
-#目前使用這個跑得出來
-
-dev.off()
-
-# nolint end
-
+# write the combined matrix to a CSV file
+write.csv(1 - combined_matrix, file = output_csv, row.names = TRUE)
