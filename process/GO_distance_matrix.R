51e4deca536e6b344754422298f7db6f405bf996
vincent <hankggg2003xd@gmail.com>
2024-08-24 19:30:18 +0800

    [refactor] refactor Go_distance_matrix

--- process/GO_distance_matrix.R
+++ process/GO_distance_matrix.R
@@ -1,32 +1,93 @@
 library(GOSemSim)
 library(org.Hs.eg.db)
+library(rentrez)
 
 input_file <- "breast/result/GSE89093_nc/train100/dbeta_hyper_TSS_0.02.csv"
-cluster_num <- 5
-output_pdf <- "breast/result/GSE89093_nc/train100/dendrogram.pdf"
 output_csv <- "breast/result/GSE89093_nc/train100/distance_matrix.csv"
 
+# Define utility functions
+# -------------------------
+# inspect the number of NA and NaN values in a vector
+nan_count <- function(x, data_name) {
+  cat(
+    "Number of (NA | NaN | non-NA/NaN) values in ", data_name, ":",
+    sum(is.na(x)), " | ", sum(is.nan(x)), " | ", sum(!is.na(x) & !is.nan(x)),
+    "\n"
+  )
+}
+
+# fetch official gene names from NCBI Gene
+fetch_official_gene_names <- function(gene_symbols) {
+  official_names <- sapply(gene_symbols, function(gene) {
+    search_result <- entrez_search(
+      db = "gene",
+      term = paste0(gene, "[sym]"),
+      retmax = 1
+    )
+    if (length(search_result$ids) > 0) {
+      gene_details <- entrez_summary(
+        db = "gene",
+        id = search_result$ids[1]
+      )
+      return(gene_details$name)
+    } else {
+      return(NA)
+    }
+  })
+  return(official_names)
+}
+
+# map gene symbols to Entrez IDs or vice versa
+gene_mapper <- function(keys, keytype, column) {
+  entrez_ids <- mapIds(
+    org.Hs.eg.db,
+    keys = keys,
+    keytype = keytype,
+    column = column
+  )
+  return(entrez_ids)
+}
+
+# calculate similarity between genes
+calculate_similarity <- function(d, ids) {
+  mgeneSim(ids, semData = d, drop = "", measure = "Wang", verbose = TRUE)
+}
+
+# fill the combined matrix with the similarity matrices
+fill_matrix <- function(main_matrix, sub_matrix) {
+  rows <- rownames(sub_matrix)
+  cols <- colnames(sub_matrix)
+  main_matrix[rows, cols] <- sub_matrix
+  return(main_matrix)
+}
+
+# Main script
+# -----------
 # read the input file
 data <- read.csv(input_file, header = TRUE, stringsAsFactors = FALSE)
-entrez_ids <- mapIds(
-  org.Hs.eg.db,
-  keys = data$gene,
-  keytype = "SYMBOL",
-  column = "ENTREZID"
-)
+gene_list <- data$gene
+entrez_ids <- gene_mapper(gene_list, "SYMBOL", "ENTREZID")
+nan_count(entrez_ids, "Entrez IDs")
 
-# remove NA values
-entrez_ids <- entrez_ids[!is.na(entrez_ids)]
+# replace missing Entrez IDs with official gene names if any
+official_names <- fetch_official_gene_names(data$gene[is.na(entrez_ids)])
+cat("Official names for missing Entrez IDs(source: NCBI Gene): \n")
+print(official_names)
+gene_list_modified <- gene_list[!is.na(entrez_ids)]
+gene_list_modified <- c(gene_list_modified, unname(official_names))
+gene_list_modified <- gene_list_modified[!is.na(gene_list_modified)]
 
-# load GO data for biological process (BP), cellular component (CC), and molecular function (MF)
+# map gene symbols to Entrez IDs again
+entrez_ids_modified <- gene_mapper(gene_list_modified, "SYMBOL", "ENTREZID")
+nan_count(entrez_ids_modified, "Modified Entrez IDs")
+
+# load GO data for BP, CC, and MF
 d_all <- lapply(c("BP", "CC", "MF"), function(ont) {
   godata(annoDb = "org.Hs.eg.db", ont = ont, computeIC = FALSE)
 })
 
-# calculate semantic similarity matrices
-sim_matrices <- lapply(d_all, function(d) {
-  mgeneSim(entrez_ids, semData = d, drop = "", measure = "Wang", verbose = TRUE)
-})
+# calculate similarity matrices
+sim_matrices <- lapply(d_all, calculate_similarity, ids = entrez_ids)
 sim_matrix_bp <- sim_matrices[[1]]
 sim_matrix_cc <- sim_matrices[[2]]
 sim_matrix_mf <- sim_matrices[[3]]
@@ -45,41 +106,34 @@ combined_matrix <- matrix(NA, length(all_genes), length(all_genes))
 rownames(combined_matrix) <- all_genes
 colnames(combined_matrix) <- all_genes
 
-# fill the combined matrix with the similarity matrices
-fill_matrix <- function(main_matrix, sub_matrix) {
-  rows <- rownames(sub_matrix)
-  cols <- colnames(sub_matrix)
-  main_matrix[rows, cols] <- sub_matrix
-  return(main_matrix)
-}
 combined_matrix <- fill_matrix(combined_matrix, sim_matrix_bp)
 combined_matrix <- fill_matrix(combined_matrix, sim_matrix_cc)
 combined_matrix <- fill_matrix(combined_matrix, sim_matrix_mf)
 
 # transform Entrez IDs to gene symbols using mapIds
-gene_symbols <- mapIds(
-  org.Hs.eg.db,
-  keys = row.names(combined_matrix),
-  keytype = "ENTREZID",
-  column = "SYMBOL"
+gene_symbols <- gene_mapper(row.names(combined_matrix), "ENTREZID", "SYMBOL")
+
+# replace official gene names with missing Entrez IDs if any
+official_names_vector <- official_names
+gene_symbols_restored <- ifelse(
+  gene_symbols %in% official_names_vector,
+  names(official_names_vector)[match(gene_symbols, official_names_vector)],
+  gene_symbols
 )
+gene_symbols_with_name <- setNames(gene_symbols_restored, names(gene_symbols))
 
 # update row and column names of combined_matrix
-rownames(combined_matrix) <- gene_symbols
-colnames(combined_matrix) <- gene_symbols
+rownames(combined_matrix) <- gene_symbols_with_name
+colnames(combined_matrix) <- gene_symbols_with_name
 
 # calculate the distance matrix
-distance_matrix <- as.dist(1 - combined_matrix)
+distance_matrix <- 1 - combined_matrix
 
-# check for NA, NaN, and Infinite values
-cat("Number of NA values: ", sum(is.na(distance_matrix)), "\n")
-cat("Number of NaN values: ", sum(is.nan(distance_matrix)), "\n")
-cat("Number of Infinite values: ", sum(is.infinite(distance_matrix)), "\n")
+nan_count(entrez_ids_modified, "Modified Entrez IDs")
+nan_count(gene_symbols, "Gene symbols")
+swapped_entrez_ids <- setNames(names(entrez_ids_modified), entrez_ids_modified)
+diff <- setdiff(swapped_entrez_ids, gene_symbols)
+print(diff)
 
-# replace NA, NaN, and Infinite values with 1
-distance_matrix[is.na(distance_matrix) | is.nan(distance_matrix) |
-    is.infinite(distance_matrix)
-] <- 1
-
-# write the combined matrix to a CSV file
-write.csv(1 - combined_matrix, file = output_csv, row.names = TRUE)
+# print columns of entrez_ids
+write.csv(distance_matrix, file = output_csv, row.names = TRUE)
