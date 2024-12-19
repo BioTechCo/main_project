846a7f36791857a305289c70d73f56d56132207e
vincent <hankggg2003xd@gmail.com>
2024-12-20 04:23:13 +0800

    [update] champ env

--- R-scripts/parse.R
+++ R-scripts/parse.R
@@ -1,13 +1,13 @@
 # nolint start
 library(ChAMP)
-myLoad <- champ.load("train/stage_III")#, SampleCutoff = 0.2)#, arraytype = "EPIC")
+myLoad <- champ.load("data/TCGA_COAD_READ")#, SampleCutoff = 0.2)#, arraytype = "EPIC")
 
 myNorm <- champ.norm(beta=myLoad$beta,plotBMIQ=FALSE,cores=5)#, arraytype = "EPIC")
 
-write.csv(myNorm,file="all_beta_normalized_stage_450k_1213.csv",quote=F,row.names=T)
+write.csv(myNorm,file="all_beta_normalized_TCGA_COAD_READ.csv",quote=F,row.names=T)
 
 myDMP <- champ.DMP(beta=myNorm, pheno=myLoad$pd$Sample_Group)#, arraytype = "EPIC")
 
-write.csv(myDMP[[1]], file="DMP_result_1213.csv", quote=F)
+write.csv(myDMP[[1]], file="DMP_result_TCGA_COAD_READ.csv", quote=F)
 
 # nolint end
