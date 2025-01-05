9537732b2d56d271f8436f9dc691a7f1ddcee209
vincent <hankggg2003xd@gmail.com>
2025-01-06 03:30:38 +0800

    feat: enhance function signatures with type hints and improve performance charting

--- notebooks/utils/combination_helper.py
+++ notebooks/utils/combination_helper.py
@@ -17,110 +17,178 @@ from sklearn.metrics import (
     accuracy_score,
     matthews_corrcoef,
     f1_score,
+    recall_score,
+    roc_curve,
+    auc,
+
 )
-
-def is_unique(lst):
-    return len(lst) == len(set(lst))
-
-
-def prepare_data(data, combination, label, cluster_num):
-    X = []
-    cluster_values = []
-    for i in range(cluster_num):
-
-        gene_values = data.loc[data[data.columns[0]].isin([combination[i]])]
-        gene_values = gene_values.iloc[:, 1::].values.flatten().tolist()
-        cluster_values.append(gene_values)
-
-    for i in range(len(label)):
-        sample = [cluster_values[j][i] for j in range(cluster_num)]
-        X.append(sample)
-    return X
+from typing import List
+from itertools import product
+from typing import Literal
 
 
-def predict_comb(
-    train_data, train_label, test_data, test_label, cluster_num, param_grids, final_gene
-):
-    combinations_list = list(combinations(final_gene["ID"], cluster_num))
-    result = []
-    models = {
-        "XGBoost": XGBClassifier(),
-        "RandomForest": RandomForestClassifier(),
-        "SVM": SVC(),
-        "Decision Tree": DecisionTreeClassifier(),
-    }
+class CombinationHelper:
+    def __init__(self,
+                 y_train: pd.Series,
+                 X_train: pd.DataFrame,
+                 y_test: pd.Series,
+                 X_test: pd.DataFrame,
+                 dbeta: pd.DataFrame,
+                 gene_list: List[str],
+                 param_grids: dict) -> None:
+        self.y_train = y_train
+        self.X_train = X_train
+        self.y_test = y_test
+        self.X_test = X_test
+        self.dbeta = dbeta
+        self.gene_list = gene_list
+        self.param_grids = param_grids
 
-    for model_name, model in models.items():
-        # 取出所有可能的組合
-        for combination in combinations_list:
-            lst = []
-            for i in range(cluster_num):
-                lst.append(
-                    int(
-                        final_gene.loc[final_gene["ID"] == combination[i]][
-                            "cluster"
-                        ].iloc[0]
-                    )
+        self.dbeta = self.dbeta[self.dbeta["gene"].isin(
+            self.gene_list["gene"])]
+        self.X_train = self.X_train[self.X_train['Unnamed: 0'].isin(
+            self.dbeta['ID'])]
+        self.X_test = self.X_test[self.X_test['Unnamed: 0'].isin(
+            self.dbeta['ID'])]
+
+        groups = dbeta['cluster'].unique()
+        if len(groups) < 3:
+            raise ValueError(
+                "Not all four groups are present in the DataFrame.")
+
+        genes_by_group = {
+            group: dbeta[dbeta['cluster'] == group]['gene'].tolist() for group in groups}
+
+        self.combs = [
+            combination for combination in product(*genes_by_group.values())
+            if len(set(combination)) == 4  # Ensure genes are distinct
+        ]
+
+        self.models = {
+            "XGBoost": XGBClassifier(),
+            "Random_Forest": RandomForestClassifier(),
+            "SVM": SVC(),
+            "Decision_Tree": DecisionTreeClassifier(),
+        }
+
+    def prepare_data(self, type: Literal["train", "test"] = "train", genes: tuple = None) -> list:
+        if type == "train":
+            return self.X_train[self.X_train['Unnamed: 0'].isin(genes)].iloc[:, 1:].T.values.tolist()
+        elif type == "test":
+            return self.X_test[self.X_test['Unnamed: 0'].isin(genes)].iloc[:, 1:].T.values.tolist()
+        else:
+            raise ValueError("Invalid type. Must be 'train' or 'test'.")
+
+    def set_parameters(self, model: object, model_name: str) -> object:
+        return GridSearchCV(
+            estimator=model, param_grid=self.param_grids[model_name], cv=5, n_jobs=-1, verbose=2
+        )
+
+    def append_to_file(file_name: str, data: pd.DataFrame) -> None:
+        if not os.path.isfile(file_name):
+            data.to_csv(file_name, index=False)
+        else:
+            data.to_csv(file_name, index=False, mode="a", header=False)
+
+    def record(
+        self,
+        y_pred_on_X: pd.Series,
+        y: pd.Series,
+        model_name: str,
+        best_score: float,
+        best_params: dict,
+        filepath: str,
+        filename: str,
+        combination: tuple
+    ) -> None:
+
+        tn, fp, _, _ = confusion_matrix(y, y_pred_on_X).ravel()
+        accuracy = accuracy_score(y, y_pred_on_X)
+        recall = recall_score(y, y_pred_on_X)
+        if (tn + fp) == 0:
+            print("Warning: No true negative found. Setting specificity to 0.")
+            specificity = 0
+        else:
+            specificity = tn / (tn + fp)
+        precision = precision_score(y, y_pred_on_X)
+        fpr, tpr, _ = roc_curve(y, y_pred_on_X)
+        roc_auc = auc(fpr, tpr)
+        fpr = fpr.tolist()
+        tpr = tpr.tolist()
+
+        self.append_to_file(
+            f"{filepath}/{filename}.csv",
+            pd.DataFrame(
+                [
+                    {
+                        "model": model_name,
+                        "accuracy": accuracy,
+                        "recall": recall,
+                        "specificity": specificity,
+                        "precision": precision,
+                        "f1_score": f1_score(y, y_pred_on_X),
+                        "AUC": roc_auc,
+                        "MCC": matthews_corrcoef(y, y_pred_on_X),
+                        "Mean cross-validated score": best_score,
+                        "Best parameters": best_params,
+                        **{f"ID{i+1}": gene for i, gene in enumerate(combination) if gene is not None}
+                    }
+                ]
+            ),
+        )
+        self.append_to_file(
+            f"{filepath}/{filename}_fpr_tpr.csv",
+            pd.DataFrame(
+                [
+                    {
+                        "model": model_name,
+                        "fpr": fpr,
+                        "tpr": tpr,
+                        "AUC": roc_auc,
+                    }
+                ]
+            ),
+        )
+
+    def train(self, folder: str, filename: str, discard_overfitting: bool = False):
+        for model_name, model in self.models.items():
+            for combination in self.combs:
+                y_train = self.y_train
+                y_test = self.y_test
+                X_train = self.prepare_data("train", combination)
+                X_test = self.prepare_data("test", combination)
+                model = self.set_parameters(model, model_name)
+                model.fit(X_train, y_train)
+
+                best_params = model.best_params_
+
+                best_score = model.best_score_
+                y_pred_on_X_train = model.best_estimator.predict(X_train)
+                y_pred_on_X_test = model.best_estimator.predict(X_test)
+
+                if discard_overfitting:
+                    train_acc = accuracy_score(y_train, y_pred_on_X_train)
+                    test_acc = accuracy_score(y_test, y_pred_on_X_test)
+                    if abs(train_acc - test_acc) > 0.1:
+                        print("Train accuracy: ", round(train_acc, 6))
+                        print("Test accuracy: ", round(test_acc, 6))
+                        print(f"========={model_name} overfitting =========")
+                        continue
+                self.record(
+                    y_pred_on_X_train,
+                    y_train,
+                    model_name,
+                    best_score,
+                    best_params,
+                    folder,
+                    filename
                 )
-            # 檢查是否在不同群
-            if is_unique(lst):
-                # 準備訓練 / 測試資料
-                X_test = prepare_data(test_data, combination, test_label, cluster_num)
-                X_train = prepare_data(
-                    train_data, combination, train_label, cluster_num
+                self.record(
+                    y_pred_on_X_test,
+                    y_test,
+                    model_name,
+                    best_score,
+                    best_params,
+                    folder,
+                    filename
                 )
-
-                # 模型參數調整
-                param_grid = param_grids[model_name]
-                grid_search = GridSearchCV(
-                    estimator=model, param_grid=param_grid, cv=5, n_jobs=-1
-                )
-                grid_search.fit(X_train, train_label)
-                print("Best Parameters:", grid_search.best_params_)
-                print("Best Score:", grid_search.best_score_)
-                model = grid_search.best_estimator_
-
-                # 預測
-                y_pred_train = model.predict(X_train)
-                accuracy_train = accuracy_score(train_label, y_pred_train)
-                # print("Train accuracy: ", round(accuracy_train, 2))
-                y_pred = model.predict(X_test)
-                accuracy = accuracy_score(test_label, y_pred)
-                # print("Test accuracy: ", round(accuracy, 2))
-
-                # 檢查是否過擬合
-                if abs(accuracy_train - accuracy) > 0.1:
-                    print("Train accuracy: ", round(accuracy_train, 2))
-                    print("Test accuracy: ", round(accuracy, 2))
-                    print(f"========={model_name} overfitting =========\n")
-                else:
-                    # 輸出結果
-                    tn, fp, fn, tp = confusion_matrix(test_label, y_pred).ravel()
-                    sensitivity = tp / (tp + fn)
-                    specificity = tn / (tn + fp)
-                    precision = precision_score(test_label, y_pred)
-                    f1 = f1_score(test_label, y_pred)
-                    mcc = matthews_corrcoef(test_label, y_pred)
-
-                    sorted_combination = [x for _, x in sorted(zip(lst, combination))]
-                    result.append(
-                        [model_name]
-                        + sorted_combination
-                        + [
-                            round(accuracy, 2),
-                            round(sensitivity, 2),
-                            round(specificity, 2),
-                            round(precision, 2),
-                            round(f1, 2),
-                            round(mcc, 2),
-                        ]
-                    )
-    id_columns = [f"ID{i+1}" for i in range(cluster_num)]
-    result = pd.DataFrame(
-        result,
-        columns=["Model"]
-        + id_columns
-        + ["accuracy", "sensitivity", "specificity", "precision", "f1_score", "mcc"],
-    )
-
-    return result
