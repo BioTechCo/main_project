1033e6a5562ea41153361ab64e6bd9a63da1c806
vincent <hankggg2003xd@gmail.com>
2025-01-06 15:05:04 +0800

    combination

--- notebooks/utils/combination_helper.py
+++ notebooks/utils/combination_helper.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 
-from itertools import combinations
 from sklearn.model_selection import GridSearchCV
 from xgboost import XGBClassifier
 
@@ -29,41 +28,45 @@ from typing import Literal
 
 class CombinationHelper:
     def __init__(self,
-                 y_train: pd.Series,
-                 X_train: pd.DataFrame,
-                 y_test: pd.Series,
-                 X_test: pd.DataFrame,
+                 train_df: pd.DataFrame,
+                 test_df: pd.DataFrame,
                  dbeta: pd.DataFrame,
                  gene_list: List[str],
-                 param_grids: dict) -> None:
-        self.y_train = y_train
-        self.X_train = X_train
-        self.y_test = y_test
-        self.X_test = X_test
+                 ) -> None:
+        self.train_df = train_df
+        self.test_df = test_df
         self.dbeta = dbeta
         self.gene_list = gene_list
-        self.param_grids = param_grids
+        self.models = {}
+        self.grid_estimators = {}
 
+    def setup_dbeta(self):
         self.dbeta = self.dbeta[self.dbeta["gene"].isin(
             self.gene_list["gene"])]
-        self.X_train = self.X_train[self.X_train['Unnamed: 0'].isin(
-            self.dbeta['ID'])]
-        self.X_test = self.X_test[self.X_test['Unnamed: 0'].isin(
-            self.dbeta['ID'])]
 
-        groups = dbeta['cluster'].unique()
+    def setup_train_test(self):
+        self.X_train = self.train_df[self.train_df['Unnamed: 0'].isin(
+            self.dbeta['ID'])]
+        self.X_test = self.test_df[self.test_df['Unnamed: 0'].isin(
+            self.dbeta['ID'])]
+        self.y_train = self.train_df.iloc[-1, 1:].astype(int)
+        self.y_test = self.test_df.iloc[-1, 1:].astype(int)
+
+    def setup_combinations(self):
+        groups = self.dbeta['cluster'].unique()
         if len(groups) < 3:
             raise ValueError(
                 "Not all four groups are present in the DataFrame.")
 
         genes_by_group = {
-            group: dbeta[dbeta['cluster'] == group]['gene'].tolist() for group in groups}
+            group: self.dbeta[self.dbeta['cluster'] == group]['gene'].tolist() for group in groups}
 
         self.combs = [
             combination for combination in product(*genes_by_group.values())
             if len(set(combination)) == 4  # Ensure genes are distinct
         ]
 
+    def setup_estimators(self):
         self.models = {
             "XGBoost": XGBClassifier(),
             "Random_Forest": RandomForestClassifier(),
@@ -71,20 +74,48 @@ class CombinationHelper:
             "Decision_Tree": DecisionTreeClassifier(),
         }
 
+    def setup_grids(self):
+        self.param_grids = {
+            "XGBoost": {
+                "learning_rate": [0.001, 0.01, 0.1],
+                "n_estimators": [50, 100, 200],
+                "max_depth": [2, 3, 4],
+                "subsample": [0.6, 0.7, 0.8],
+                "colsample_bytree": [0.6, 0.7, 0.8],
+                "min_child_weight": [40, 50, 60],
+            },
+            "Random_Forest": {
+                "n_estimators": [50, 100, 150, 200],
+                "min_samples_split": [80, 90],
+                "min_samples_leaf": [80, 90],
+                "max_features": [5, 10, 20],
+            },
+            "SVM": {"kernel": ["rbf", "poly", "sigmoid"], "C": [50, 100, 200]},
+            "Decision_Tree": {
+                "min_samples_split": [70, 80, 90],
+                "min_samples_leaf": [70, 80, 90],
+                "max_features": [5, 10, 20],
+            },
+        }
+
+    def setup_grid_estimator(self):
+        for model_name, model in self.models.items():
+            self.grid_estimators[model_name] = GridSearchCV(
+                estimator=model, param_grid=self.param_grids[model_name], cv=5, n_jobs=-1, verbose=2
+            )
+
+    def get_ID(self, genes: tuple) -> tuple:
+        return tuple(self.dbeta[self.dbeta['gene'].isin(genes)]['ID'])
+
     def prepare_data(self, type: Literal["train", "test"] = "train", genes: tuple = None) -> list:
         if type == "train":
-            return self.X_train[self.X_train['Unnamed: 0'].isin(genes)].iloc[:, 1:].T.values.tolist()
+            return self.X_train[self.X_train['Unnamed: 0'].isin(list(self.get_ID(genes)))].iloc[:, 1:].T.values.tolist()
         elif type == "test":
-            return self.X_test[self.X_test['Unnamed: 0'].isin(genes)].iloc[:, 1:].T.values.tolist()
+            return self.X_test[self.X_test['Unnamed: 0'].isin(list(self.get_ID(genes)))].iloc[:, 1:].T.values.tolist()
         else:
             raise ValueError("Invalid type. Must be 'train' or 'test'.")
 
-    def set_parameters(self, model: object, model_name: str) -> object:
-        return GridSearchCV(
-            estimator=model, param_grid=self.param_grids[model_name], cv=5, n_jobs=-1, verbose=2
-        )
-
-    def append_to_file(file_name: str, data: pd.DataFrame) -> None:
+    def append_to_file(self, file_name: str, data: pd.DataFrame) -> None:
         if not os.path.isfile(file_name):
             data.to_csv(file_name, index=False)
         else:
@@ -134,7 +165,7 @@ class CombinationHelper:
                         **{f"ID{i+1}": gene for i, gene in enumerate(combination) if gene is not None}
                     }
                 ]
-            ),
+            )
         )
         self.append_to_file(
             f"{filepath}/{filename}_fpr_tpr.csv",
@@ -147,28 +178,25 @@ class CombinationHelper:
                         "AUC": roc_auc,
                     }
                 ]
-            ),
+            )
         )
 
-    def train(self, folder: str, filename: str, discard_overfitting: bool = False):
-        for model_name, model in self.models.items():
+    def train(self, train_folder: str,test_folder: str, filename: str, discard_overfitting: bool = False):
+        for model_name, grid_estimator in self.grid_estimators.items():
             for combination in self.combs:
-                y_train = self.y_train
-                y_test = self.y_test
-                X_train = self.prepare_data("train", combination)
-                X_test = self.prepare_data("test", combination)
-                model = self.set_parameters(model, model_name)
-                model.fit(X_train, y_train)
+                prepared_X_train = self.prepare_data("train", combination)
+                prepared_X_test = self.prepare_data("test", combination)
+                grid_estimator.fit(prepared_X_train, self.y_train)
 
-                best_params = model.best_params_
+                best_params = grid_estimator.best_params_
 
-                best_score = model.best_score_
-                y_pred_on_X_train = model.best_estimator.predict(X_train)
-                y_pred_on_X_test = model.best_estimator.predict(X_test)
+                best_score = grid_estimator.best_score_
+                y_pred_on_X_train = grid_estimator.best_estimator_.predict(prepared_X_train)
+                y_pred_on_X_test = grid_estimator.best_estimator_.predict(prepared_X_test)
 
                 if discard_overfitting:
-                    train_acc = accuracy_score(y_train, y_pred_on_X_train)
-                    test_acc = accuracy_score(y_test, y_pred_on_X_test)
+                    train_acc = accuracy_score(self.y_train, y_pred_on_X_train)
+                    test_acc = accuracy_score(self.y_test, y_pred_on_X_test)
                     if abs(train_acc - test_acc) > 0.1:
                         print("Train accuracy: ", round(train_acc, 6))
                         print("Test accuracy: ", round(test_acc, 6))
@@ -176,19 +204,21 @@ class CombinationHelper:
                         continue
                 self.record(
                     y_pred_on_X_train,
-                    y_train,
+                    self.y_train,
                     model_name,
                     best_score,
                     best_params,
-                    folder,
-                    filename
+                    train_folder,
+                    filename,
+                    combination
                 )
                 self.record(
                     y_pred_on_X_test,
-                    y_test,
+                    self.y_test,
                     model_name,
                     best_score,
                     best_params,
-                    folder,
-                    filename
+                    test_folder,
+                    filename,
+                    combination
                 )
